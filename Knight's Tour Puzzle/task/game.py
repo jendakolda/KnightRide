0a887ff3195593684cad212d72a2fd509f0bb902
Já <email@jendakolda.cz>
2021-10-01 22:55:24 +0200

    Stage 2 not working

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -1,5 +1,5 @@
 # Write your code here
-from numpy import chararray
+import numpy as np
 
 
 class KnightRider:
@@ -14,44 +14,38 @@ class KnightRider:
             if two_numbers and pos_digits:
                 self.columns, self.rows = map(int, board_dim)
                 break
+        self.row_marker = len(str(self.rows))
+        # self.col_marker = len(str(self.columns))
         self.cell = len(str(self.columns * self.rows))
-        self.field = chararray((self.rows, self.columns), itemsize=self.cell, order='F')
+        self.field = np.chararray((self.rows, self.columns), itemsize=self.cell, order='F')
         self.field[:] = self.cell * '_'
-        # print(self.field)
-        # self.rows = rows
-        # self.columns = columns
 
     def print_field(self):
-
-
-        print(' ' + (self.columns*(self.cell + 1) + 3) * '-')
+        print(self.row_marker * ' ' + (self.columns * (self.cell + 1) + 3) * '-')
         for i in range(self.rows):
-            prefix = len(str(self.rows)) - len(str(i))
-            print(prefix)
-            print(f'{(len(str(self.rows)) - len(str(i))) * " "} {self.rows - i}'|', *self.field[i].decode('utf-8'), '|', sep=' ')
-        print('  ' + (self.columns*(self.cell + 1) + 3) * '-')
-        print('  ', *(i + 1 for i in range(self.columns)))
+            prefix = self.row_marker - len(str(self.rows - i))
+            print(prefix * ' ' + f'{self.rows - i}|', *self.field[i].decode('utf-8'), '|', sep=' ')
+        print(self.row_marker * ' ' + (self.columns * (self.cell + 1) + 3) * '-')
+        numbers_below = (self.row_marker + 1) * ' '
+        for i in range(self.columns):
+            numbers_below += (self.cell - len(str(i + 1))) * ' ' + ' ' + str(i + 1)
+        print(numbers_below)
 
     def get_start(self):
-        starting_pos = input('Enter the knight\'s starting position: ').split()
+        while True:
+            starting_pos = input('Enter the knight\'s starting position: ').split()
+            two_numbers = len(starting_pos) == 2
+            all_digits = all(i.isdigit() for i in starting_pos)
+            is_inrange = int(starting_pos[0]) - 1 in range(self.columns) and int(starting_pos[1]) - 1 in range(self.rows)
 
-        two_numbers = len(starting_pos) == 2
-        all_digits = all(i.isdigit() for i in starting_pos)
-
-        if not two_numbers or not all_digits:
-            print('Invalid dimensions!')
-            quit()
-
-        is_inrange = int(starting_pos[0]) - 1 in range(self.columns) \
-            and int(starting_pos[1]) - 1 in range(self.rows)
-
-        if is_inrange:
-            start_column, start_row = map(int, starting_pos)
-            start_column -= 1
-            start_row = self.rows - start_row
-            self.field[start_row, start_column] = (self.cell - 1) * ' ' + 'X'
-        else:
-            print('Invalid position!')
+            if two_numbers and all_digits and is_inrange:
+                start_column, start_row = map(int, starting_pos)
+                start_column -= 1
+                start_row = self.rows - start_row
+                self.field[start_row, start_column] = (self.cell - 1) * ' ' + 'X'
+                break
+            else:
+                print('Invalid position!')
 
 
 if __name__ == '__main__':
@@ -59,4 +53,3 @@ if __name__ == '__main__':
     jezdec.get_start()
     jezdec.print_field()
 
-bytes()
