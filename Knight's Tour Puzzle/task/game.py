e9f3f0ab9ffab2caf245982ddfecaeced3dbcb50
Já <email@jendakolda.cz>
2021-10-02 20:46:39 +0200

    Stage 3 finished

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -20,6 +20,14 @@ class KnightRider:
         self.cell = len(str(self.columns * self.rows))
         self.field = np.chararray((self.rows, self.columns), itemsize=self.cell, order='F')
         self.field[:] = self.cell * '_'
+        self.current_column = None
+        self.current_row = None
+
+    def mark_cell(self, row, column, sign):
+        self.field[row, column] = (self.cell - 1) * ' ' + sign
+
+    def range_check(self, checked_row, checked_column):
+        return checked_row in range(self.rows) and checked_column in range(self.columns)
 
     def print_field(self):
         print(self.row_marker * ' ' + (self.columns * (self.cell + 1) + 3) * '-')
@@ -40,21 +48,29 @@ class KnightRider:
 
             if not two_numbers or not all_digits:
                 print('Invalid position!')
-                break
-            is_inrange = int(starting_pos[0]) - 1 in range(self.columns) and int(starting_pos[1]) - 1 in range(self.rows)
-
-            if two_numbers and all_digits and is_inrange:
-                start_column, start_row = map(int, starting_pos)
-                start_column -= 1
-                start_row = self.rows - start_row
-                self.field[start_row, start_column] = (self.cell - 1) * ' ' + 'X'
+            elif self.range_check(int(starting_pos[1]) - 1, int(starting_pos[0]) - 1):
+                self.current_column, self.current_row = map(int, starting_pos)
+                self.current_column -= 1
+                self.current_row = self.rows - self.current_row
+                self.mark_cell(self.current_row, self.current_column, 'X')
+                self.field[self.current_row, self.current_column] = (self.cell - 1) * ' ' + 'X'
                 break
             else:
                 print('Invalid position!')
+
+    def get_possible_moves(self):
+        moves = ((- 1, -2,), (-2, -1), (1, -2), (-2, 1), (-1, 2), (2, -1), (1, 2), (2, 1))
+        # possible_moves = []
+        for move in moves:
+            new_pos = (self.current_row + move[0], self.current_column + move[1])
+            if self.range_check(new_pos[0], new_pos[1]):
+                self.mark_cell(new_pos[0], new_pos[1], 'O')
+                # possible_moves.append(new_pos)
 
 
 if __name__ == '__main__':
     jezdec = KnightRider()
     jezdec.get_start()
+    jezdec.get_possible_moves()
     jezdec.print_field()
 
