1d2c621d60ac53755186b656ec3313b41e4e2790
Já <email@jendakolda.cz>
2021-10-03 21:20:54 +0200

    Stage 5 incomplete

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -41,20 +41,28 @@ class KnightRider:
             numbers_below += (self.cell - len(str(i + 1))) * ' ' + ' ' + str(i + 1)
         print(numbers_below)
 
-    def get_start(self):
+    def clear_field(self):
+        with np.nditer(self.field, op_flags=['readwrite']) as field:
+            for i in field:
+                if str(i, 'utf-8').lstrip(' ').isdigit():
+                    i[...] = self.cell * '_'
+                elif 'X' in str(i, 'utf-8'):
+                    i[...] = (self.cell - 1) * ' ' + '*'
+
+    def get_move_coords(self, text):
         while True:
-            starting_pos = input('Enter the knight\'s starting position: ').split()
-            two_numbers = len(starting_pos) == 2
-            all_digits = all(i.isdigit() for i in starting_pos)
+            move_coords = input(text).split()
+            two_numbers = len(move_coords) == 2
+            all_digits = all(i.isdigit() for i in move_coords)
 
             if not two_numbers or not all_digits:
                 print('Invalid position!')
-            elif self.range_check(int(starting_pos[1]) - 1, int(starting_pos[0]) - 1):
-                self.current_column, self.current_row = map(int, starting_pos)
+            elif self.range_check(int(move_coords[1]) - 1, int(move_coords[0]) - 1):
+                self.current_column, self.current_row = map(int, move_coords)
                 self.current_column -= 1
                 self.current_row = self.rows - self.current_row
                 self.mark_cell(self.current_row, self.current_column, 'X')
-                self.field[self.current_row, self.current_column] = (self.cell - 1) * ' ' + 'X'
+
                 break
             else:
                 print('Invalid position!')
@@ -69,11 +77,18 @@ class KnightRider:
         return possible_moves
 
     def main(self):
-        self.get_start()
-        possible_moves = self.get_possible_moves(self.current_row, self.current_column)
-        for move in possible_moves:
-            self.mark_cell(move[0], move[1], str(len(self.get_possible_moves(move[0], move[1]))))
-        self.print_field()
+        while True:
+            if self.current_row and self.current_column:
+                prompt_text = 'Enter your next move: '
+            else:
+                prompt_text = 'Enter the knight\'s starting position: '
+            self.get_move_coords(prompt_text)
+            possible_moves = self.get_possible_moves(self.current_row, self.current_column)
+
+            for move in possible_moves:
+                self.mark_cell(move[0], move[1], str(len(self.get_possible_moves(move[0], move[1]))))
+            self.print_field()
+            self.clear_field()
 
 
 if __name__ == '__main__':
